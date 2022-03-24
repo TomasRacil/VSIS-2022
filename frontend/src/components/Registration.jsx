d92a945cc6fdf277c7fa636baf0d942bbc8b3a9d
Uživatelské jméno <z.vylezich.ml@seznam.cz>
2022-03-24 09:29:34 +0000

    HTML registrace

---
+++ frontend/src/components/Registration.jsx
@@ -0,0 +1,36 @@
+
+const Registrace = () => {
+    return (
+      <nav className="registrace">
+        <h1>Tour de Kosova</h1>
+          Jmeno
+          Prijmeni
+          Vek
+          Pohlavi
+          Email 
+
+          <thead>
+                <tr>
+                    <th>Jmeno</th>
+                    <th>Prijmeni</th>
+                    <th>Vek</th>
+                    <th>Pohlavi</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>xxx</td>
+                    <td>yyy</td>
+                    <td>25</td>
+                    <td>Man</td>
+                    <td>xxx.yyy@unob.cz</td>
+                </tr>
+            </tbody>
+      </nav>
+
+      
+    );
+  };
+  
+  export default Registrace;
