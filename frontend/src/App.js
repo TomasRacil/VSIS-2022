e5868fa27644d4654d43e508efc0d32b8d05cc58
PetrGallus <gallus-petr@seznam.cz>
2022-03-30 11:56:14 +0000

    startovni_listina_v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Body from './components/Body';
 import Registration from './components/Registration';
+import StartovniListina from './components/StartovniListina';
+import Table, {SelectColumnFilter} from './components/Table'
 
 function App() {
   return (
     <div>
       {/* <NavBar></NavBar>
-      <Body></Body> */}
-      <Registration></Registration>
+      <Body></Body>
+      <Registration></Registration>*/}
+      <StartovniListina></StartovniListina>
     </div>
 
     // <h1>Hello world</h1>
   );
 }
+
+
 
 export default App;
