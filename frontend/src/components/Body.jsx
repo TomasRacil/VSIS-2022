e8b7effc4800b03587bf4d6bcec437a5a47b347b
Microtuss <kubaja00@gmail.com>
2022-03-31 08:17:57 +0000

    Body

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,9 +1,23 @@
-<<<<<<< HEAD
-import {Accordion} from 'react-bootstrap'
+import {Accordion, Card, Button} from 'react-bootstrap'
+import UO from "./Logo_of_UO.svg.png"
+import Flag from "./Flag_of_Kosovo.svg.png"
 const Body = () => {
 
   return (<div>
 <h1>Tour de kosova</h1>
+<Card className="text-center">
+  <Card.Header>Mapa</Card.Header>
+  <Card.Body>
+    <Card.Title>Tour de Kosovo</Card.Title>
+    <Card.Text>
+      Zde si prohlédněte mapku celého závodu
+    </Card.Text>
+    <center><img src={Flag} class="center" alt="Tour de Kosovo" width="150" height="150" ></img></center>
+    <Button variant="primary" a href="https://www.google.com/maps/d/viewer?ie=UTF8&hl=en&msa=0&ll=42.65012200000002%2C20.742188&spn=3.232133%2C3.713379&z=6&source=embed&mid=1QA-ent10r6q_RRp7vKXTOKzrlV8"> Mapa</Button>
+  </Card.Body>
+</Card>
+
+
 <Accordion defaultActiveKey={['0']} alwaysOpen>
   <Accordion.Item eventKey="0">
     <Accordion.Header>Nějaky nadpis</Accordion.Header>
@@ -30,27 +44,6 @@ const Body = () => {
     </Accordion.Body>
   </Accordion.Item>
 </Accordion>
-</div>)
-=======
-import { Card, Button} from "react-bootstrap";
-import UO from "./Logo_of_UO.svg.png"
-import Flag from "./Flag_of_Kosovo.svg.png"
-const Body = () => {
-  return (<div>
-  <h1>Hello world</h1>
-
-<Card className="text-center">
-  <Card.Header>Mapa</Card.Header>
-  <Card.Body>
-    <Card.Title>Tour de Kosovo</Card.Title>
-    <Card.Text>
-      Zde si prohlédněte mapku celého závodu
-    </Card.Text>
-    <center><img src={Flag} class="center" alt="Tour de Kosovo" width="150" height="150" ></img></center>
-    <Button variant="primary" a href="https://www.google.com/maps/d/viewer?ie=UTF8&hl=en&msa=0&ll=42.65012200000002%2C20.742188&spn=3.232133%2C3.713379&z=6&source=embed&mid=1QA-ent10r6q_RRp7vKXTOKzrlV8"> Mapa</Button>
-  </Card.Body>
-</Card>
-
 <Card className="text-center">
   <Card.Header>Sponzoři</Card.Header>
   <Card.Body>
@@ -63,9 +56,6 @@ const Body = () => {
   </Card.Body>
   <Card.Footer className="text-muted"></Card.Footer>
 </Card>
-
-  </div>);
->>>>>>> f322e2ce19bd9e8f07be96deb7f1b4420a763cf2
+</div>);
 };
-
 export default Body;
