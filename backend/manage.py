ed0eb74b6eef5559140cc16e59fa6be2653e910f
TomasRacil <tomas.racil@unob.cz>
2022-03-31 22:47:47 +0000

    Backend template extended

---
+++ backend/manage.py
@@ -0,0 +1,20 @@
+import os
+import click
+
+from flask_migrate import Migrate
+
+from app import blueprint
+from app.main import create_app, db
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
+app.app_context().push()
+migrate = Migrate(app, db)
+
+
+# @app.cli.command("start")
+# def start():
+#     app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
